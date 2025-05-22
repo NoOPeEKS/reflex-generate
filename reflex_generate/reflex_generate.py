d9502623cf42c2c62e779902804e4875207f2642
NoOPeEKS <arnauapps@gmail.com>
2025-05-23 00:21:47 +0200

    feat: scaffold command skeleton

--- reflex_generate/reflex_generate.py
+++ reflex_generate/reflex_generate.py
@@ -1,5 +1,33 @@
+"""Reflex-Generate CLI to create HTTP Resources for Reflex apps."""
+
+import sys
+from importlib import metadata
+from importlib.util import find_spec
+
+import click
+
+
+def check_dependencies():
+    """Checks if reflex is installed as a dependency."""
+    if not find_spec("reflex"):
+        sys.stdout.write("You must have `reflex` installed before using this app\n")
+        exit(1)
+
+
+@click.group()
+@click.version_option(metadata.version("reflex-generate"), message="%(version)s")
 def cli():
-    print("Hello from reflex-generate!")
+    """Reflex-Generate CLI to create HTTP Resources for Reflex apps."""
+    check_dependencies()
+    pass
+
+
+@cli.command()
+@click.argument("model_name", nargs=1)
+@click.argument("fields", nargs=-1)
+def scaffold(model_name: str, fields: tuple[str, ...]):
+    """Subcommand for managing the scaffolding of resources."""
+    pass
 
 
 if __name__ == "__main__":
